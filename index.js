989e558c848bc0e4e1e6afcd8fd4021c7a4834cb
Colin Reist <94681686+colin-reist@users.noreply.github.com>
2023-10-04 19:43:00 +0200

    Version final de la soirée

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection(); // permet de faire fonctionner les cooldowns des commandes
 const foldersPath = path.join(__dirname, 'commands/.');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -27,12 +28,40 @@ client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
+client.on(Event.messageReationAdd, async (reaction, user) => {
+	if (reaction.emoji.name === '👍') {
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); 
+	}
+});
+
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
+	const { cooldowns } = client;
 	const command = client.commands.get(interaction.commandName);
 
 	if (!command) return;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
 	try {
 		await command.execute(interaction);
