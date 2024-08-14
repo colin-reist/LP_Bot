6d79120fea67dd408bc221899ee7284c93440aa6
Colin Reist <colinreist04@gmail.com>
2024-08-14 03:43:18 +0200

    V 1.2

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { Client, Collection, Events, GatewayIntentBits, Partials, ActivityType, E
 const { token } = require('./MainConfig.json');
 const cron = require('cron');
 const { Tags, Booster, suggestion, userLevels } = require('./database.js');
+const memberUpdate = require('./events/guildMemberUpdate.js');
+const messageReactionAdd = require('./events/messageReactionAdd.js');
+const interactionCreated = require('./events/interactionCreate.js');
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMembers],
@@ -192,7 +195,7 @@ client.on(Events.MessageCreate, async (message) => {
 	// Si le message est envoyé par le bot de bump et que la commande est bump
 	if (message.interaction.commandName === commandName) {
 		// eslint-disable-next-line no-useless-escape
-		const codeText = '\`/Bump\`';
+		const codeText = '\/Bump\'';
 		message.channel.send('Merci d\'avoir bump le serveur <@' + message.interaction.user.id + '> !' + '\nNous vous rappelerons dans 2 heures de bump le serveur !');
 		setTimeout(() => {
 			message.channel.send('Il est temps de Bump ! <@&1044348995901861908> !');
@@ -217,243 +220,30 @@ client.on(Events.MessageCreate, async (message) => {
  * @returns
  */
 client.on(Events.GuildMemberUpdate, async (oldMember, newMember) => {
-	const guild = newMember.guild;
-	const userId = newMember.user.id;
-
 	if (!oldMember.roles.cache.has('965755928974618735') && newMember.roles.cache.has('965755928974618735')) {
-		// User has boosted the server
-		const userBoost = await Booster.findOne({ where: { userId } });
-
-		if (userBoost) {
-			// User has boosted the server more than once
-			userBoost.boostCount++;
-			userBoost.save();
-
-			const embed = new EmbedBuilder()
-				.setTitle('Un booster a augmenté son nombre de boost !')
-				.setDescription(`Merci, ${newMember.user.tag}, pour booster le serveur ${userBoost.boostCount} fois ! Nous te sommes reconnaissants pour tes nombreux boosts.`)
-				.setColor('#EBBC4E');
-
-			const channel = guild.channels.cache.get('1061643658723590164');
-
-			if (channel) {
-				channel.send({ embeds: [embed] });
-			}
-		} else {
-			// User is boosting the server for the first time
-			await Booster.create({ userId, boostCount: 1 });
-
-			const embed = new EmbedBuilder()
-				.setTitle('Nouveau Booster!')
-				.setDescription(`Bienvenue, ${newMember.user.tag}, merci d'avoir booster le serveur ! Nous apprécions ton soutien.`)
-				.setColor('#EBBC4E');
-
-			const channel = guild.channels.cache.get('1061643658723590164');
-
-			if (channel) {
-				channel.send({ embeds: [embed] });
-			}
-		}
-	} else if (oldMember.roles.cache.has('965755928974618735') && !newMember.roles.cache.has('965755928974618735')) {
-		// User has removed a boost
-		const userBoost = await Booster.findOne({ where: { userId } });
-
-		if (userBoost) {
-			userBoost.boostCount--;
-
-			if (userBoost.boostCount <= 0) {
-			// If the user has no more boosts, delete the row from the database
-				await userBoost.destroy();
-			} else {
-				userBoost.save();
-			}
-		}
+		memberUpdate.boost(newMember);
 	}
 });
 
-
-/**
-	 * Capte le rajout d'une réaction sur un message
-	 * @param {MessageReaction} reaction La réaction ajoutée
-	 */
 client.on(Events.MessageReactionAdd, async (reaction) => {
-	// When a reaction is received, check if the structure is partial
-	// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
 	try {
 		await reaction.fetch();
 	}
 	catch (error) {
 		console.error('Une erreur est survenue lors d\'un rajout d\'émoji: ', error);
 	}
-	checkReaction(reaction, 'add');
+	messageReactionAdd.checkReaction(reaction, 'add');
 });
 
-/**
-	 * Capte le retrait d'une réaction sur un message
-	 * @param {MessageReaction} reaction La réaction retirée
-	 */
 client.on(Events.MessageReactionRemove, async (reaction) => {
-	// When a reaction is received, check if the structure is partial
-	// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
 	try {
 		await reaction.fetch();
 	}
 	catch (error) {
 		console.error('Something went wrong when fetching the message:', error);
-		// Return as `reaction.message.author` may be undefined/null
 	}
-
-	checkReaction(reaction, 'remove');
+	messageReactionAdd.checkReaction(reaction, 'remove');
 });
-
-async function checkReaction(reaction, addOrRemove) {
-	if (reaction.message.channel.id === '1079499858064441344' || reaction.message.channel.id === '1153607344505245736') {
-		starboard(reaction, addOrRemove);
-	}
-}
-
-/* async function suggestionManager(reaction, positive, negative) {
-	console.log('------suggestion manager------');
-
-	const existingSuggestion = await suggestion.findOne({ where: { suggestionId: reaction.message.id } });
-	if (existingSuggestion === null) {
-		console.log(' -> La suggestion n\'existe pas, id: ' + reaction.message.id);
-		return;
-	}
-
-	existingSuggestion.suggestionCountTrue = positive;
-	existingSuggestion.suggestionCountFalse = negative;
-	existingSuggestion.save();
-
-	if (existingSuggestion.suggestionImage === '') {
-		try {
-			const specialEmbed = new EmbedBuilder()
-				.setColor('#ff0000')
-				.addFields(
-					{ name: 'Votes', value: '\n✅' + existingSuggestion.suggestionCountTrue + '\n❌' + existingSuggestion.suggestionCountFalse + '\n' },
-					{ name: 'Suggestion', value: existingSuggestion.suggestionSuggestion },
-				)
-				.setTimestamp();
-			await reaction.message.edit({ embeds: [specialEmbed] });
-		} catch (error) {
-			console.error(error);
-		}
-		return;
-	} else {
-		try {
-			const specialEmbed = new EmbedBuilder()
-				.setColor('#ff0000')
-				.addFields(
-					{ name: 'Votes', value: '\n✅' + existingSuggestion.suggestionCountTrue + '\n❌' + existingSuggestion.suggestionCountFalse + '\n' },
-					{ name: 'Suggestion', value: existingSuggestion.suggestionSuggestion },
-				)
-				.setTimestamp()
-				.setImage(existingSuggestion.suggestionImage);
-			await reaction.message.edit({ embeds: [specialEmbed] });
-		} catch (error) {
-			console.error(error);
-		}
-		console.log(' -> Modification de l\'embed');
-	}
-	console.log('-------------------------------');
-}*/
-
-/**
- * Fonction qui gère le starboard
- * @param {*} reaction La réaction ajoutée ou retirée
- * @param {*} AddOrRemove Si la réaction a été ajoutée ou retirée
- * @returns
- */
-async function starboard(reaction, AddOrRemove) {
-	console.log('-------------starboard-------------');
-
-
-	try {
-		let existingTag = await Tags.findOne({ where: { linkedEmbed: reaction.message.id } });
-		let messageAttachment = null; // initialize messageAttachment to null
-
-		if (existingTag === null) {
-			existingTag = await Tags.findOne({ where: { messageID: reaction.message.id } });
-			if (existingTag === null) {
-				console.log('-------Création du tag-------');
-
-
-				// Check if the message has an image attachment
-				if (reaction.message.attachments.size > 0) {
-					const attachment = reaction.message.attachments.first();
-					if (attachment.contentType.startsWith('image/')) {
-						messageAttachment = attachment.url;
-					}
-				}
-				// If a tag doesn't already exist, create one
-				// eslint-disable-next-line no-unused-vars
-				const tag = await Tags.create({
-					messageID: reaction.message.id,
-					messageAuthorName: reaction.message.author.username,
-					messageAuthorId: reaction.message.author.id,
-					messageAuthorAvatar: reaction.message.author.displayAvatarURL(),
-					messageURL: reaction.message.url,
-					reactCount: reaction.count,
-					attachment: messageAttachment,
-					posted: false,
-					linkedEmbed: null,
-				});
-				return console.log(' Contenu du tag: \n' + 'MessageID: ' + tag.messageID + '\nMessageAuthorName: '
-					+ tag.messageAuthorName + '\nMessageAuthorId: '	+ tag.messageAuthorId + '\nMessageAuthorAvatar: ' + tag.messageAuthorAvatar
-					+ '\nMessageURL: ' + tag.messageURL + '\nReactCount: ' + tag.reactCount + '\nAttachment: '
-					+ tag.attachment + '\nPosted: ' + tag.posted + '\nLinkedEmbed: ' + tag.linkedEmbed + '\n -> Tag créé \n---------------------------');
-
-			} else {
-				console.log(' -> Tag existant sans embed');
-			}
-		} else {
-			console.log(' -> Tag existant avec embed');
-		}
-
-		(AddOrRemove === 'add') ? existingTag.reactCount++ : existingTag.reactCount--;
-		existingTag.save();
-
-		const realReactCount = existingTag.reactCount - 1 ; // On retire 1 au compteur de réaction pour éviter de compter le bot
-		const starboardEmbed = new EmbedBuilder()
-			.setColor('#EBBC4E')
-			.setTitle('🌟 ' + realReactCount + ' | de ' + existingTag.messageURL)
-			.setAuthor({ name: existingTag.messageAuthorName, iconURL: existingTag.messageAuthorAvatar, url: existingTag.messageURL })
-			.setImage(existingTag.attachment)
-			.setFooter({ text: 'Message ID: ' + existingTag.messageID });
-
-		const starboardChannel = client.channels.cache.get('1153607344505245736'); // le channel du starboard
-		// Si le message n'a pas encore été posté et qu'il a plus de 15 réactions, on poste un nouvel embed
-		if (!existingTag.posted && existingTag.reactCount > 15) {
-			console.log(' -> Création de l\'embed');
-			existingTag.posted = true;
-			existingTag.save();
-			const message = await starboardChannel.send({ embeds: [starboardEmbed] });
-			await message.react('🌟');
-			const sendMessageID = message.id;
-			existingTag.linkedEmbed = sendMessageID;
-			existingTag.save();
-
-			// Si le message a déjà été posté et qu'il a plus de 15 réactions, on modifie l'embed
-		} else if (existingTag.posted && existingTag.reactCount > 14) {
-			console.log(' -> Modification de l\'embed');
-			const message = await starboardChannel.messages.fetch(existingTag.linkedEmbed);
-			message.edit({ embeds: [starboardEmbed] });
-
-			// Si le message a déjà été posté et qu'il a moins de 15 réactions, on supprime l'embed
-		} else if (existingTag.posted && existingTag.reactCount < 15) {
-			console.log(' -> Suppression de l\'embed');
-			existingTag.posted = false;
-			existingTag.save();
-			const message = await starboardChannel.messages.fetch(existingTag.linkedEmbed);
-			message.delete();
-
-		}
-	}
-	catch (error) {
-		console.error('Une erreur est survenue lors d\'un rajout d\'émoji: ', error);
-	}
-	console.log('-----------------------------------');
-}
 
 /**
  * Capte les interactions
@@ -461,205 +251,7 @@ async function starboard(reaction, AddOrRemove) {
  * @returns
  */
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const { cooldowns } = client;
-	const command = client.commands.get(interaction.commandName);
-
-	const { commandName } = interaction;
-
-	if (commandName === 'addtag') {
-		console.log('addtag');
-		const tagName = interaction.options.getString('name');
-		const tagDescription = interaction.options.getString('description');
-
-		try {
-			console.log('-------Création du tag-------');
-			// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-			const tag = await Tags.create({
-				messageID: tagName,
-				description: tagDescription,
-				username: interaction.user.username,
-			});
-			console.log(' -> Tag créé \n' + ' Contenu du tag: \n' + 'MessageID: ' + tag.messageID + '\nMessageAuthorName: ');
-			return interaction.reply(`Tag ${tag.messageID} added.`);
-		}
-		catch (error) {
-			if (error.name === 'SequelizeUniqueConstraintError') {
-				return interaction.reply('That tag already exists.');
-			}
-
-			return interaction.reply('Something went wrong with adding a tag.');
-		}
-	}
-	else if (commandName === 'tag') {
-		console.log('tag');
-		const tagName = interaction.options.getString('name');
-
-		// equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-		const tag = await suggestion.findOne({ where: { suggestionId: tagName } });
-
-		if (tag) {
-			// equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
-			tag.increment('usage_count');
-
-			return interaction.reply(tag.get('description'));
-		}
-
-		return interaction.reply(`Could not find tag: ${tagName}`);
-	}
-	else if (commandName === 'edittag') {
-		console.log('edittag');
-		const tagName = interaction.options.getString('name');
-		const tagDescription = interaction.options.getString('description');
-
-		// equivalent to: UPDATE tags (description) values (?) WHERE name='?';
-		const affectedRows = await Tags.update({ description: tagDescription }, { where: { name: tagName } });
-
-		if (affectedRows > 0) {
-			return interaction.reply(`Tag ${tagName} was edited.`);
-		}
-
-		return interaction.reply(`Could not find a tag with name ${tagName}.`);
-	}
-	else if (commandName == 'taginfo') {
-		console.log('taginfo');
-		const tagName = interaction.options.getString('name');
-
-		// equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-		const tag = await suggestion.findOne({ where: { suggestionId: tagName } });
-
-		if (tag) {
-			return interaction.reply(`${tagName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`);
-		}
-
-		return interaction.reply(`Could not find tag: ${tagName}`);
-	}
-	else if (commandName === 'showtags') {
-		// equivalent to: SELECT name FROM tags;
-		console.log('showtags');
-		const tagList = await suggestion.findAll({ attributes: ['suggestionId'] });
-		const tagString = tagList.map(t => t.suggestionId).join(', ') || 'No tags set.';
-
-		return interaction.reply(`List of tags: ${tagString}`);
-	}
-	else if (commandName === 'deletetag') {
-		console.log('deletetag');
-		const tagName = interaction.options.getString('name');
-		// equivalent to: DELETE from tags WHERE name = ?;
-		const rowCount = await Tags.destroy({ where: { messageID: tagName } });
-
-		if (!rowCount) return interaction.reply('That tag doesn\'t exist.');
-
-		return interaction.reply('Tag deleted.');
-	}
-	else if (commandName === 'resettag') {
-		console.log('resettag');
-		Tags.sync({ force: true });
-
-		return interaction.reply('Tags reset.');
-	} else if (commandName === 'suggérer') {
-		const actualSuggestions = await suggestion.findOne({ where: { suggestionId: interaction.id } });
-		console.log(actualSuggestions);
-		if (actualSuggestions === null) {
-			let messages = null;
-			if (interaction.options.getAttachment('image') === null) {
-				try {
-					console.log('création de l\'embed sans image');
-					const embed = new EmbedBuilder()
-						.setColor('#EBBC4E')
-						.setTitle('✨ Construction de votre suggestion ✨');
-
-					const channelID = interaction.channel;
-					messages = await channelID.send({ embeds: [embed] });
-				} catch (error) {
-					console.error(error);
-				}
-			} else {
-				try {
-					console.log('création de l\'embed avec image');
-					const embed = new EmbedBuilder()
-						.setColor('#EBBC4E')
-						.setTitle('✨ Construction de votre suggestion ✨');
-					const channelID = interaction.channel;
-					messages = await channelID.send({ embeds: [embed] });
-				} catch (error) {
-					console.error(error);
-				}
-			}
-
-			console.log('Rajout dans la table');
-			const suggestionId = messages.id;
-			const suggestionName = interaction.options.getString('nom');
-			const suggestionSuggestion = interaction.options.getString('suggestion');
-			const suggestionSuggerant = interaction.options.getUser('suggerant').id;
-			const suggestionCount = 0;
-			let existingImage = null;
-
-			if (interaction.options.getAttachment('image') === null) {
-				existingImage = '';
-			} else {
-				existingImage = interaction.options.getAttachment('image').url;
-			}
-
-			try {
-				console.log('-------Création de la suggestion-------');
-				// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-				const tag = await suggestion.create({
-					suggestionId: suggestionId,
-					suggestionName: suggestionName,
-					suggestionSuggestion: suggestionSuggestion,
-					suggestionSuggerant: suggestionSuggerant,
-					suggestionCountTrue: suggestionCount,
-					suggestionCountFalse: suggestionCount,
-					suggestionImage: existingImage,
-				});
-				console.log('Contenu de la suggestion: \n' + 'suggestionId: ' + tag.suggestionId + '\nsuggestionName: '
-				+ tag.suggestionName + '\nsuggestionDescription: ' + tag.suggestionSuggestion
-				+ '\nsuggestionSuggerant: ' + tag.suggestionSuggerant + '\nsuggestionCount: ' + tag.suggestionCount + '\nexistingImage: '
-				+ tag.suggestionImage + '\n---------------------------');
-			} catch (error) {
-				console.error(error);
-			}
-			await messages.react('✅');
-			await messages.react('❌');
-		}
-	}
-
-	if (!command) return;
-
-	if (!cooldowns.has(command.data.name)) {
-		cooldowns.set(command.data.name, new Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.data.name);
-	const defaultCooldownDuration = 3;
-	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-	if (timestamps.has(interaction.user.id)) {
-		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const expiredTimestamp = Math.round(expirationTime / 1000);
-			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
-		}
-	}
-
-	timestamps.set(interaction.user.id, now);
-	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+	interactionCreated.interactionCreate(interaction);
 });
-module.exports = { userLevels };
+
 client.login(token);
