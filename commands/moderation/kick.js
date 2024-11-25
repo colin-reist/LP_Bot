6a88b44b9c0ecac497a731801456f4c31a7fd401
Colin Reist <colinreist04@gmail.com>
2024-11-25 21:48:28 +0100

    Refactor ban and kick commands to improve user input handling and error messaging

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,128 +1,110 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { kicks, badUsers: badUserModel, staffMembers } = require('../../database.js');
 
 module.exports = {
-	category: 'moderation',
+    category: 'moderation',
     data: new SlashCommandBuilder()
         .setName('kick')
-        .addUserOption(option => option.setName('utilisateur').setDescription('L\'utilisateur à kick').setRequired(true))
-        .addStringOption(option => option.setName('raison').setDescription('La raison du kick').setRequired(true))
-        .setDescription('Kick un utilisateur du serveur'),
+        .setDescription('Kick un utilisateur du serveur')
+        .addStringOption(option => 
+            option.setName('utilisateur')
+                .setDescription('Mention ou ID de l\'utilisateur à kick')
+                .setRequired(true))
+        .addStringOption(option => 
+            option.setName('raison')
+                .setDescription('La raison du kick')
+                .setRequired(true)),
     async execute(interaction) {
-        // Check if the user has the required role
+        const userInput = interaction.options.getString('utilisateur');
+        const raison = interaction.options.getString('raison');
+        const staff = interaction.member.user;
+
+        // Vérifie si l'utilisateur a le rôle requis
         const requiredRole = interaction.guild.roles.cache.find(role => role.name === 'Staff');
         if (!interaction.member.roles.cache.has(requiredRole.id)) {
-            return interaction.reply({ content: 'You do not have the required role to use this command.', ephemeral: true });
+            return interaction.reply({ content: 'Vous n\'avez pas les permissions pour utiliser cette commande.', ephemeral: true });
         }
-    
-        await interaction.reply({ content: 'Kicking user...', ephemeral: true });
-    
-        // kick le membre puis enregistre le kick dans la base de données
-        // Capture le staff executant la commande
-		const staff = interaction.member.user;
-        const staffId = staff.id;
 
-		// Capture la personne visée par la commande
-		const user = interaction.options.getUser('utilisateur');
-        const member = await interaction.guild.members.fetch(user.id);
+        await interaction.reply({ content: 'Traitement du kick en cours...', ephemeral: true });
 
-		// Check si le staff executant la commande est un staff
-        const staffMember = await staffMembers.findOne({ where: { sm_user_id: staffId } });
-        if (!staffMember) {
-            return interaction.editReply({ content: 'Tu n\'es pas un staff', ephemeral: true });
+        try {
+            // Recherche de l'utilisateur par mention ou ID
+            const user = userInput.startsWith('<@') 
+                ? interaction.options.getUser('utilisateur') 
+                : await interaction.client.users.fetch(userInput).catch(() => null);
+
+            if (!user) {
+                return interaction.editReply({ content: 'Utilisateur introuvable.', ephemeral: true });
+            }
+
+            const member = await interaction.guild.members.fetch(user.id);
+
+            // Vérifie si l'utilisateur est un membre du staff
+            const staffMember = await staffMembers.findOne({ where: { sm_user_id: staff.id } });
+            if (!staffMember) return interaction.editReply({ content: 'Vous devez être staff pour utiliser cette commande.', ephemeral: true });
+            
+            const isStaff = await staffMembers.findOne({ where: { sm_user_id: user.id } });
+            if (isStaff) return interaction.editReply({ content: 'Vous ne pouvez pas kicker un autre membre du staff.', ephemeral: true });
+
+            // Vérifie et ajoute l'utilisateur à la liste des mauvais utilisateurs si nécessaire
+            const [badUser, created] = await badUserModel.findOrCreate({
+                where: { bu_id: user.id },
+                defaults: { bu_id: user.id, bu_name: user.username }
+            });
+
+            // Enregistre le kick dans la base de données
+            await kicks.create({
+                ki_reason: raison,
+                ki_date: new Date(),
+                ki_fk_badUsers: badUser.pk_badUsers,
+                ki_fk_staffMembers: staffMember.pk_staffMembers
+            });
+
+            // Envoie un message de kick à l'utilisateur
+            const embedToUser = new EmbedBuilder()
+                .setColor('#FF0000')
+                .setTitle('Kick')
+                .setDescription('Vous avez été kické du serveur')
+                .addFields(
+                    { name: 'Raison', value: raison },
+                    { name: 'Staff', value: staff.username }
+                )
+                .setTimestamp()
+                .setThumbnail(staff.displayAvatarURL());
+
+            try {
+                await member.send({ embeds: [embedToUser] });
+            } catch (error) {
+                console.error('Impossible d\'envoyer le message au membre :', error);
+            }
+
+            // Kick l'utilisateur du serveur
+            await member.kick(raison);
+            await interaction.editReply({ content: `L'utilisateur <@${user.id}> a été kické pour la raison suivante : ${raison}` });
+
+            // Envoie un message dans le canal de log
+            const logChannel = interaction.guild.channels.cache.find(channel => channel.name === 'kick-log');
+            if (logChannel) {
+                const embedToLog = new EmbedBuilder()
+                    .setColor('#FF0000')
+                    .setTitle('Kick')
+                    .setDescription('Un utilisateur a été kické')
+                    .addFields(
+                        { name: 'Utilisateur', value: user.username },
+                        { name: 'Raison', value: raison },
+                        { name: 'Staff', value: staff.username }
+                    )
+                    .setTimestamp()
+                    .setThumbnail(user.displayAvatarURL());
+                
+                logChannel.send({ embeds: [embedToLog] });
+            } else {
+                console.warn('Le canal de log "kick-log" est introuvable.');
+            }
+
+        } catch (error) {
+            console.error('Erreur lors du processus de kick :', error);
+            interaction.editReply({ content: 'Une erreur est survenue lors du kick de l\'utilisateur.' });
         }
-		
-		const isStaff = await staffMembers.findOne({ where: { sm_user_id: user.id } });
-		if (isStaff) {
-			return interaction.reply({ content: 'You can\'t kick a staff member.', ephemeral: true });
-		}
-
-		// Check si l'utilisateur est déjà sur la liste des mauvais utilisateurs
-		// Si l'utilisateur n'y est pas on le rajoute à la liste de badUsers
-		const badUser = await badUserModel.findOne({ where: { bu_id: user.id } });
-		if (!badUser) {
-			try {
-				await badUserModel.create({
-					bu_id: user.id,
-					bu_name: user.username,
-				});
-			} catch (SequelizeUniqueConstraintError) {
-				console.log('Constrainte de clé unique violée sur la table badUsers');
-			}
-		}
-
-		// Capture la fk de l'utilisateur warni pour lier le warn à l'utilisateur
-        const foundBadUser = await badUserModel.findOne({ where: { bu_id: user.id } });
-        const fkBadUser = foundBadUser.pk_badUsers;
-
-        // Capture la fk du staff executant la commande pour lier le warn au staff
-        const staffMemberId = await staffMembers.findOne({ where: { sm_user_id: staffId } });
-        const fkStaffMember = staffMemberId.pk_staffMembers;
-
-		// Capture la raison du kick
-		const raison = interaction.options.getString('raison');
-
-		// Rajout du kick dans la base de donneés
-		try {
-			await kicks.create({
-				ki_reason: raison,
-				ki_date: new Date(),
-				ki_fk_badUsers: fkBadUser,
-				ki_fk_staffMembers: fkStaffMember,
-			});
-		} catch (error) {
-			console.log('Erreur lors du rajout du kick dans la base de données');
-		}
-
-		try {
-			await interaction.editReply({ content: `L'utilisateur <@${user.id}> a été kick pour la raison suivante : ${raison}` });
-		} catch (error) {
-			console.log(error);
-			return;
-		}
-
-		try {
-				// Envoie un embed d'avertissement à l'utilisateur
-			const embedToUser = new EmbedBuilder()
-				.setColor('#FF0000')
-				.setTitle('Kick')
-				.setDescription('Vous avez été kick')
-				.addFields(
-					{ name: 'Raison', value: raison },
-					{ name: 'Staff', value: staff.username },
-				)
-				.setTimestamp()
-				.setThumbnail(staff.displayAvatarURL());
-			member.send({ embeds: [embedToUser] });
-		} catch (error) {
-			console.log('Erreur lors de l\'envoie du messsage à l\'utilisateur' + error);
-		}
-		
-		try {
-			// kick l'utilisateur
-			await member.kick({ reason: raison });
-		} catch (error) {
-			console.log('Erreur lors du kick de l\'utilisateur' + error);
-		}
-
-		
-		try {
-			// Envoie un embed dans le channel de modération
-			const embedToLog = new EmbedBuilder()
-				.setColor('#FF0000')
-				.setTitle('Kick')
-				.setDescription('Un utilisateur a été kick')
-				.addFields(
-					{ name: 'Utilisateur', value: member.user.username },
-					{ name: 'Raison', value: raison },
-					{ name: 'Staff', value: staff.username },
-				)
-				.setTimestamp()
-				.setThumbnail(member.user.displayAvatarURL());
-				const channel = interaction.guild.channels.cache.find(channel => channel.name === 'kick-log');
-			channel.send({ embeds: [embedToLog] });
-		} catch (error) {
-			console.log('Erreur lors de l\'envoie du message dans le channel de modération' + error);
-		}
-    },
+    }
 };
