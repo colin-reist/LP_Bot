caacde54f0a3e9e3cdba3aab2a4fa6198eee43f9
Colin Reist <94681686+colin-reist@users.noreply.github.com>
2025-01-26 16:35:59 +0100

    error handling

--- events/message/rectionAdd.js
+++ events/message/rectionAdd.js
@@ -3,10 +3,16 @@ const { Events } = require('discord.js'); // Importer Events
 module.exports = (client) => {
     client.on(Events.MessageReactionAdd, async (reaction) => {
         try {
+            if (!reaction.message.guild) return;
+
+            const botPermissions = reaction.message.guild.me.permissionsIn(reaction.message.channel);
+            if (!botPermissions.has('VIEW_CHANNEL') || !botPermissions.has('READ_MESSAGE_HISTORY')) {
+                throw new error('The bots does not have the required permissions to view the channel or read the message history');
+            }
             await reaction.fetch();
         }
         catch (error) {
-            console.error('Une erreur est survenue lors d\'un rajout d\'émoji: ', error);
+            console.error('reactionAdd error : ', error);
         }
         // messageReactionAdd.checkReaction(reaction, 'add');
     });
