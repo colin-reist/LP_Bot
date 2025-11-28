ef15052f89c5a0f58fee097e526487be994981ae
Colin Reist <94681686+colin-reist@users.noreply.github.com>
2025-11-28 15:03:45 +0100

    Add more logs when crashing

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -9,6 +9,7 @@ module.exports = (client) => {
 
 	process.on('uncaughtException', async (error) => {
 		logger.error('An unCaugth error has been detected :', error.stack?? error);
+		logger.error(error.message?? 'No message available');
 
 		try {
 			const errorEmbed = new EmbedBuilder()
@@ -28,6 +29,7 @@ module.exports = (client) => {
 			// Remplace "ID_DU_CHANNEL" par l'ID du channel où tu veux envoyer le message
 			const channel = client.channels.cache.get(crashLogsChannelId);
 			if (channel && channel.isTextBased()) {
+				await channel.send('<@551091502860730368> Tu sais pas coder')
 				await channel.send({ embeds: [errorEmbed] });
 			}
 		} catch (err) {
